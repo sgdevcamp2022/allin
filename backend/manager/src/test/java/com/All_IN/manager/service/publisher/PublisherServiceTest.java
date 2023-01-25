329a0098e62a436b14f0fa5aabe9e6377fb5ac56
belljun3395 <195850@jnu.ac.kr>
2023-01-26 03:49:35 +0900

    feat(manager/publisher): publisher 관련 테스트 구현
    - PublisherServiceTest
    - PublisherServiceTestException
    - schema.sql

---
+++ backend/manager/src/test/java/com/All_IN/manager/service/publisher/PublisherServiceTest.java
@@ -0,0 +1,138 @@
+package com.All_IN.manager.service.publisher;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@Slf4j
+@SpringBootTest
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
+class PublisherServiceTest {
+
+    @Autowired
+    private PublisherService publisherService;
+
+    private static Long publisherId = 1L;
+
+    @BeforeEach
+    void save_data() {
+        publisherService.save(999L);
+    }
+
+
+    @AfterEach
+    void clear() {
+        publisherService.clear();
+        publisherId++;
+    }
+
+    @Test
+    void save() {
+
+        // Given
+
+        // When
+        publisherService.save(1L);
+
+        // Then
+        publisherId++;
+
+    }
+
+    @Test
+    void getKey() {
+        // Given
+
+        // When
+        String key = publisherService.getKey(publisherId);
+        log.info("key = {}", key);
+
+        // Then
+
+    }
+
+    @Test
+    void generatePassword() {
+        // Given
+
+        // When
+        String password = publisherService.generatePassword(publisherId);
+        log.info("password = {}", password);
+
+        // Then
+
+    }
+
+    @Test
+    void resetPassword() {
+        // Given
+        String password_before = publisherService.generatePassword(publisherId);
+        log.info("password_before = {}", password_before);
+
+        // When
+        publisherService.resetPassword(publisherId);
+
+        String password_after = publisherService.generatePassword(publisherId);
+        log.info("password_after = {}", password_after);
+
+        // Then
+        Assertions.assertThat(password_before.equals(password_after)).isFalse();
+    }
+
+    @Test
+    void generateURL() {
+        // Given
+        publisherService.generatePassword(publisherId);
+        String key = publisherService.getKey(publisherId);
+
+        // When
+        String url = publisherService.generateURL(publisherId);
+        log.info("url = {}", url);
+
+        // Then
+        Assertions.assertThat(url).isEqualTo(key + "?pw=");
+    }
+
+    @Test
+    void validatePublisher() {
+        // Given
+        String password = publisherService.generatePassword(publisherId);
+        String key = publisherService.getKey(publisherId);
+
+        // When
+        publisherService.validatePublisher(key, password);
+
+        // Then
+    }
+
+    @Test
+    void usePassword() {
+        // Given
+        String password = publisherService.generatePassword(publisherId);
+
+        // When
+        publisherService.usePassword(password);
+
+        // Then
+    }
+
+    @Test
+    void updateKey() {
+        // Given
+        String key_before = publisherService.getKey(publisherId);
+
+        // When
+        publisherService.updateKey(publisherId);
+        String key_after = publisherService.getKey(publisherId);
+
+        // Then
+        Assertions.assertThat(key_before.equals(key_after)).isFalse();
+    }
+
+}
---
+++ backend/manager/src/test/java/com/All_IN/manager/service/publisher/PublisherServiceTestException.java
@@ -0,0 +1,207 @@
+package com.All_IN.manager.service.publisher;
+
+import com.All_IN.manager.service.publisher.PublisherService;
+import com.All_IN.manager.service.publisher.exception.PublisherServiceException;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@Slf4j
+@SpringBootTest
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
+class PublisherServiceTestException {
+
+    @Autowired
+    private PublisherService publisherService;
+
+    private static Long publisherId = 1L;
+
+    @BeforeEach
+    void save_data() {
+        publisherService.save(1L);
+    }
+
+
+    @AfterEach
+    void clear() {
+        publisherService.clear();
+        publisherId++;
+    }
+
+    @Test
+    void save_EXIST_PUBLISHER() {
+        // Given
+
+        // When
+        Assertions.assertThatThrownBy(() -> {
+            publisherService.save(1L);
+            publisherId++;
+        }).hasMessage(
+            PublisherServiceException.EXIST_PUBLISHER.getMessage());
+
+        // Then
+
+    }
+
+    @Test
+    void getKey_NO_SUCH_PUBLISHER() {
+        // Given
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.getKey(2L))
+            .hasMessage(PublisherServiceException.NO_SUCH_PUBLISHER.getMessage());
+    }
+
+    @Test
+    void generatePassword_NO_SUCH_PUBLISHER() {
+        // Given
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.generatePassword(2L))
+            .hasMessage(PublisherServiceException.NO_SUCH_PUBLISHER.getMessage());
+    }
+
+    @Test
+    void generatePassword_ALREADY_GENERATE_PASSWORD() {
+        // Given
+        publisherService.generatePassword(publisherId);
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.generatePassword(publisherId))
+            .hasMessage(PublisherServiceException.ALREADY_GENERATE_PASSWORD.getMessage());
+    }
+
+    @Test
+    void resetPassword_NO_SUCH_PUBLISHER() {
+        // Given
+        String password_before = publisherService.generatePassword(publisherId);
+        log.info("password_before = {}", password_before);
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.resetPassword(2L))
+            .hasMessage(PublisherServiceException.NO_SUCH_PUBLISHER.getMessage());
+    }
+
+    @Test
+    void resetPassword_NO_PASSWORD() {
+        // Given
+        String password_before = publisherService.generatePassword(publisherId);
+        log.info("password_before = {}", password_before);
+
+        // When
+        publisherService.usePassword(password_before);
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.resetPassword(publisherId))
+            .hasMessage(PublisherServiceException.NO_PASSWORD.getMessage());
+    }
+
+    @Test
+    void generateURL_NOT_EQUAL_URL() {
+        // Given
+        publisherService.generatePassword(publisherId);
+        String key = "random";
+
+        // When
+        String url = publisherService.generateURL(publisherId);
+        log.info("url = {}", url);
+
+        // Then
+        Assertions.assertThat(url).isNotEqualTo(key + "?pw=");
+    }
+
+    @Test
+    void generateURL_NO_SUCH_PUBLISHER() {
+        // Given
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.generateURL(2L))
+            .hasMessage(PublisherServiceException.NO_SUCH_PUBLISHER.getMessage());
+    }
+
+    @Test
+    void generateURL_NO_PASSWORD() {
+        // Given
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.generateURL(publisherId))
+            .hasMessage(PublisherServiceException.NO_PASSWORD.getMessage());
+    }
+
+    @Test
+    void validatePublisher_NO_SUCH_PUBLISHER() {
+        // Given
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.validatePublisher("key", "password"))
+            .hasMessage(PublisherServiceException.NO_SUCH_PUBLISHER.getMessage());
+    }
+
+    @Test
+    void validatePublisher_NO_PASSWORD() {
+        // Given
+        String key = publisherService.getKey(publisherId);
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.validatePublisher(key, "password"))
+            .hasMessage(PublisherServiceException.NO_PASSWORD.getMessage());
+    }
+
+    @Test
+    void validatePublisher_NO_MATCH_PASSWORD() {
+        // Given
+        String password = publisherService.generatePassword(publisherId);
+        String key = publisherService.getKey(publisherId);
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.validatePublisher(key, "password"))
+            .hasMessage(PublisherServiceException.NO_MATCH_PASSWORD.getMessage());
+    }
+
+    @Test
+    void usePassword_NO_MATCH_PASSWORD() {
+        // Given
+        String password = publisherService.generatePassword(publisherId);
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.usePassword("password"))
+            .hasMessage(PublisherServiceException.NO_MATCH_PASSWORD.getMessage());
+    }
+
+    @Test
+    void updateKey_NO_SUCH_PUBLISHER() {
+        // Given
+
+        // When
+
+        // Then
+        Assertions.assertThatThrownBy(() -> publisherService.getKey(2L))
+            .hasMessage(PublisherServiceException.NO_SUCH_PUBLISHER.getMessage());
+    }
+}
