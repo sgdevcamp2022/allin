84d4c2ddb6711d268fc93338e18d9268ef4def78
hayoung <hyng0115@gmail.com>
2023-01-27 18:36:38 +0900

    feat(채팅): 채팅방 조회 구현

---
+++ src/main/java/com/example/chat/dto/ChatMessage.java
@@ -0,0 +1,29 @@
+package com.example.chat.dto;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PRIVATE)
+public class ChatMessage implements Serializable {
+
+  private static final long serialVersionUID = 6494678977089006639L;
+  @NotEmpty
+  @Size(max = 100, message = "메시지 글자 수는 최대 100자입니다.")
+  private String message;
+
+  @JsonCreator
+  private ChatMessage(String message) {
+    this.message = message;
+  }
+
+  public static ChatMessage from(String message) {
+    return new ChatMessage(message);
+  }
+}
