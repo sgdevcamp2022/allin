3cec0d8f90090759d4053fb6dc0948dbf3c0247c
loopy <loopy@jnu.ac.kr>
2023-01-31 12:30:58 +0000

    feat(front): react-router-dom을 추가한다

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
 import App from './App'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
