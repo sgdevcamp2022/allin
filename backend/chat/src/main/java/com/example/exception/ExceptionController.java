394d54af069e63c3e6e2b29110b8b75b2be354b2
hayoung <hyng0115@gmail.com>
2023-02-02 15:29:10 +0900

    chore(채팅): 실패 응답 형식 변경

--- backend/chat/src/main/java/com/example/exception/ExceptionController.java
+++ backend/chat/src/main/java/com/example/exception/ExceptionController.java
@@ -1,6 +1,8 @@
 package com.example.exception;
 
 
+import static com.example.exception.ErrorMessage.REQUEST_DATA_NOT_FOUND;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
@@ -13,12 +15,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseBody
 @Slf4j
 public class ExceptionController {
-
+  private static final String SERVER_NUM = "5";
   @ExceptionHandler(BindException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ExceptionResponse handleBindException(
-    BindException e) {
+  public ExceptionResponse handleBindException(BindException e) {
     log.warn("handleBindException: {}", e);
-    return ExceptionResponse.from(e.getMessage());
+    return ExceptionResponse.of(calculateCode(HttpStatus.BAD_REQUEST,
+        REQUEST_DATA_NOT_FOUND.getCode()),
+      e.getClass().getName(), REQUEST_DATA_NOT_FOUND.getMessage());
+  }
+
+  private String calculateCode(HttpStatus status, String code) {
+    return status.value() + SERVER_NUM + code;
   }
 }
--- backend/chat/src/main/java/com/example/exception/ExceptionResponse.java
+++ backend/chat/src/main/java/com/example/exception/ExceptionResponse.java
@@ -1,5 +1,7 @@
 package com.example.exception;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionResponse {
 
+  private final Timestamp timestamp;
+  private final String code;
+  private final String error;
   private final String message;
 
-  public static ExceptionResponse from(String message) {
-    return new ExceptionResponse(message);
+  public static ExceptionResponse of(String code, String error, String message) {
+    return new ExceptionResponse(Timestamp.valueOf(LocalDateTime.now()), code, error, message);
   }
 }
