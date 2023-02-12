98ad3fba5cf2078a87164b2dab8e08e9b4e0706a
hayoung <hyng0115@gmail.com>
2023-02-12 15:51:14 +0900

    chore(신고): 예외 처리 클래스 이름, 로직 수정

--- backend/report/src/main/java/com/example/exception/ExceptionMessage.java
+++ backend/report/src/main/java/com/example/exception/ExceptionMessage.java
@@ -6,9 +6,18 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-public enum ErrorMessage {
+public enum ExceptionMessage {
   REQUEST_DATA_NOT_VALID("001", "요청 데이터가 올바르지 않습니다.");
 
   private final String code;
   private final String message;
+
+  public static ExceptionMessage findByMessage(String message) {
+    for (ExceptionMessage e : values()) {
+      if (e.getMessage().equals(message)) {
+        return e;
+      }
+    }
+    return null;
+  }
 }
