6d4f11e6393e03fcd8fe28b3b447c51162af8f86
loopy <loopy@jnu.ac.kr>
2023-02-15 04:20:41 +0000

    feat(video): add video module
    Signed-off-by: loopy <loopy@jnu.ac.kr>

--- frontend/src/Elements/Video/Video.element.tsx
+++ frontend/src/Elements/Video/Video.element.tsx
@@ -1,30 +1,36 @@
+import { useEffect, useRef } from 'react'
 import ChannelElement from '../Channal/Channal.element'
-
 const VideoElement = () => {
-  // var video = document.getElementById('video');
-  // var videoSrc = 'http://192.168.0.12:8088/hls/a53d70e0b32bdcb1fc3a674773dd2674_low/index.m3u8';
-  // if (Hls.isSupported()) {
-  //     var hls = new Hls();
-  //     hls.loadSource(videoSrc);
-  //     hls.attachMedia(video);
-  //     hls.on(Hls.Events.MANIFEST_PARSED, function () {
-  //         video.muted = 'muted';
-  //         video.autoplay = 'autoplay';
-  //         video.playsinline = 'true';
-  //         video.play();
-  //     });
-  // } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-  //     video.src = videoSrc;
-  //     video.addEventListener('loadedmetadata', function () {
-  //         video.muted = 'muted';
-  //         video.autoplay = 'autoplay';
-  //         video.playsinline = 'true';
-  //         video.play();
-  //     });
-  // }
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    // const video = videoRef.current as HTMLVideoElement
+    // const videoSrc = 'http://192.168.0.12:8088/hls/a53d70e0b32bdcb1fc3a674773dd2674_low/index.m3u8'
+    // if (video == undefined) return
+    // if (Hls.isSupported()) {
+    //   const hls = new Hls()
+    //   hls.loadSource(videoSrc)
+    //   hls.attachMedia(video)
+    //   hls.on(Hls.Events.MANIFEST_PARSED, function () {
+    //     video.muted = true
+    //     video.autoplay = true
+    //     video.playsInline = true
+    //     video.play()
+    //   })
+    // } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    //   video.src = videoSrc
+    //   video.addEventListener('loadedmetadata', function () {
+    //     video.muted = true
+    //     video.autoplay = true
+    //     video.playsInline = true
+    //     video.play()
+    //   })
+    // }
+  })
 
   return (
     <div className="pr-8">
+      {/* <video ref={videoRef}></video> */}
       <img src="http://via.placeholder.com/1920x1080" alt="" />
       <ChannelElement />
     </div>
