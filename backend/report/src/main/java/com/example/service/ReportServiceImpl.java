0d75bb7f3b93f8910c3f5560874c0f5bf0944798
hayoung <hyng0115@gmail.com>
2023-02-21 10:43:16 +0900

    refactor(신고): 신고하기 로직 리팩토링

--- backend/report/src/main/java/com/example/service/ReportServiceImpl.java
+++ backend/report/src/main/java/com/example/service/ReportServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.service;
 
 import com.example.domain.Report;
 import com.example.dto.ReportRequest;
-import com.example.repository.ReportCountRepository;
 import com.example.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,21 +10,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class ReportServiceImpl implements ReportService {
+
   private static final long THRESHOLD = 10;
   private final ReportRepository reportRepository;
-  private final ReportCountRepository reportCountRepository;
 
   private final ReportProducer reportProducer;
 
   @Override
-  @Transactional
-  public void report(ReportRequest request) {
-    long count = reportCountRepository.increaseReportCount(request.getTopicId(),
-      request.getReportedUser());
-    sendMessage(count, request.getTopicId(), request.getReportedUser());
-    Report report = Report.of(request.getReportedUser(), request.getReporter(), request.getMessage(), request.getReason());
-    reportRepository.save(report);
+  // dirty checking
+  public synchronized void report(ReportRequest request) {
+    transactionalReport(request);
   }
+
+  // @Transactional과 synchrozied를 함께 적용하기 위해 사용하는 메서드입니다.
+  @Transactional
+  protected void transactionalReport(ReportRequest request) {
+    reportRepository.findByTopicIdAndReportedUser(request.getTopicId(), request.getReportedUser())
+                    .ifPresentOrElse(
+                      report -> {
+                        report.increaseCount();
+                        sendMessage(report.getCount(), request.getTopicId(),
+                          request.getReportedUser());
+                      },
+                      () -> reportRepository.save(
+                        Report.of(request.getTopicId(), request.getReportedUser(),
+                          request.getReporter(), request.getMessage(), request.getReason())));
+  }
+
 
   private void sendMessage(long count, String topicId, String reportedUser) {
     if (count == THRESHOLD) {
